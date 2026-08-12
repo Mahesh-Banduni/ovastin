f9666ecaa3f531cb0c8164109a364f165104a4a4
Mahesh Banduni <BL0159@bliss.local>
2026-08-12 12:00:41 +0530

    updated code

--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -7,7 +7,6 @@ import { useState } from "react"
 const HeaderLinks =[
     {name: 'Home', link: '/'},
     {name: 'About', link: '/about'},
-    {name: 'Projects', link: '/projects'},
     {name: 'Services', link: '/services'},
     {name: 'Contact', link: '/contact'},
 ]
